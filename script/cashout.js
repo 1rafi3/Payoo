379b292cdcbef3ace9e35a251e152311591e599d
Rafi <rwolkorimrafi@gmail.com>
2026-02-21 17:25:06 +0600

    homepage section and cashout section is done

---
+++ script/cashout.js
@@ -0,0 +1,50 @@
+document.getElementById('cashout-btn')
+.addEventListener('click',function(){
+
+    //1 --> get the agent number & validity
+
+    const givenNumberInput = document.getElementById('input-mobile');
+    const agentNumberInput = givenNumberInput.value;
+    console.log(agentNumberInput);
+
+    //2 --> get the amount,validate, convert to number
+
+    const cashoutAmountInput = document.getElementById('cashout-amount');
+    const amountInput = cashoutAmountInput.value;
+    console.log(amountInput);
+
+    //3 --> get the current balance amount,validate, convert to number
+
+    const currBalance = document.getElementById('balance');
+    const currentBalance = currBalance.innerText;
+    console.log(currentBalance);
+
+
+    //4 --> calculate the balance 
+
+    const calculatedBalance = Number(currentBalance - amountInput);
+    
+    if (calculatedBalance < 0){
+        alert('Invalid Amount');
+        return;
+    }
+    console.log(calculatedBalance);
+    
+    //5 --> get the bill verify 
+
+    const cashoutPin = document.getElementById('cashout-pin');
+    const pin = cashoutPin.value;
+
+    if(pin == '1234'){
+        //5-1 --> true::: show an alert > set Balance 
+        alert(`"Successfully cashout : ", ${amountInput}`);
+        document.getElementById('balance').innerText = calculatedBalance;
+    } else {
+        //5-2 --> true::: show an error alert > return
+        alert('Invalid Pin');
+        return;
+    }
+
+
+
+})
--- script/login.js
+++ script/login.js
@@ -1,12 +1,23 @@
 console.log("angta lege geche");
 
 document.getElementById('login-btn')
-.addEventListener('click',function(event){
+.addEventListener('click',function(){
     //1 -> get the mobile number input 
-    //2 -> get the pin input
-    //3 -> match pin & mobile number 
-    //3-1 if truee::: -> alert>homepage
-    //3-1 if false::: -> alert>return
     const mobileNumber = document.getElementById('input-mobile');
-    console.log(mobileNumber);
+    const givenNumber = mobileNumber.value;
+    console.log(givenNumber);
+    //2 -> get the pin input
+    const pinNumber = document.getElementById('input-pin');
+    const givenPin = pinNumber.value;
+    console.log(givenPin);
+    //3 -> match pin & mobile number 
+    if(givenNumber == '0123456789' && givenPin == '1234') {
+        //3-1 if truee::: -> alert>homepage
+        alert('login Success');
+        window.location.assign("/homepage.html");
+    } else{
+        //3-1 if false::: -> alert>return
+        alert('login failed');
+        return;
+    }
 })
