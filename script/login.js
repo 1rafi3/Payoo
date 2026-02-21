19e2fb8c3be4e1dfb54a3e8e612ce7049adbfa76
Rafi <rwolkorimrafi@gmail.com>
2026-02-21 13:50:49 +0600

    project initiated with all the resources and login interface frontend done

---
+++ script/login.js
@@ -0,0 +1,12 @@
+console.log("angta lege geche");
+
+document.getElementById('login-btn')
+.addEventListener('click',function(event){
+    //1 -> get the mobile number input 
+    //2 -> get the pin input
+    //3 -> match pin & mobile number 
+    //3-1 if truee::: -> alert>homepage
+    //3-1 if false::: -> alert>return
+    const mobileNumber = document.getElementById('input-mobile');
+    console.log(mobileNumber);
+})
